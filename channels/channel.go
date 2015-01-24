630efdb9298d7b8320ae15084f4b24cbddb291a4
Niclas Jern <niclas@walkbase.com>
2015-01-24 12:46:25 +0200

    fleshed out the message structs

--- channels/channel.go
+++ channels/channel.go
@@ -1,5 +1,8 @@
 package channels
 
+// A Channel has one or more subscribers
+// and a queue of messages (which should)
+// hopefully be empty most of the time
 type Channel struct {
 	id           uint32
 	subscribers  map[uint32]struct{}
--- channels/message.go
+++ channels/message.go
@@ -1,7 +1,36 @@
 package channels
 
-type Message struct {
+// A Message is sent on a channel, either as
+// a broadcast or directly to one or more users
+// on the channel
+type Message interface {
+	Content() string
+	Receivers() map[uint32]struct{} // nil if broadcast
+}
+
+// A BroadcastMessage is broadcasted to one or more channels
+type BroadcastMessage struct {
+	content string
+}
+
+func (b *BroadcastMessage) Content() string {
+	return b.content
+}
+
+func (b *BroadcastMessage) Receivers() map[uint32]struct{} {
+	return nil
+}
+
+// A PrivateMessage is sent to one or more subscribers in a channel
+type PrivateMessage struct {
 	content   string
-	broadcast bool
 	receivers map[uint32]struct{}
+}
+
+func (p *PrivateMessage) Content() string {
+	return p.content
+}
+
+func (p *PrivateMessage) Receivers() map[uint32]struct{} {
+	return p.receivers
 }
