528b8a50782465e69103ec349579a83b9b0e7dcb
Niclas Jern <niclas@walkbase.com>
2015-01-24 12:50:32 +0200

    golint

--- channels/message.go
+++ channels/message.go
@@ -13,10 +13,12 @@ type BroadcastMessage struct {
 	content string
 }
 
+// Content returns the message content
 func (b *BroadcastMessage) Content() string {
 	return b.content
 }
 
+// Receivers returns nil for a Broadcast message
 func (b *BroadcastMessage) Receivers() map[uint32]struct{} {
 	return nil
 }
@@ -27,10 +29,12 @@ type PrivateMessage struct {
 	receivers map[uint32]struct{}
 }
 
+// Content returns the message content
 func (p *PrivateMessage) Content() string {
 	return p.content
 }
 
+// Receivers returns a map of the receiver user IDs
 func (p *PrivateMessage) Receivers() map[uint32]struct{} {
 	return p.receivers
 }
